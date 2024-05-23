356da8c7de65f0751e3a70131a30fca7b7e47a92
Mahdi <mah454@mail.moke.ir>
2024-05-23 15:19:19 +0330

    Add module 02-timer-task

---
+++ 02-timer-task/src/main/java/com/sample/module/timer/ModuleRunner.java
@@ -0,0 +1,21 @@
+package com.sample.module.timer;
+
+import ir.moke.jos.api.JModule;
+
+import java.util.Timer;
+
+public class ModuleRunner implements JModule {
+    private static final TaskService taskService = new TaskService();
+    private static Timer timer;
+
+    @Override
+    public void start() {
+        timer = new Timer("jos time module");
+        timer.scheduleAtFixedRate(taskService, 1000, 3000);
+    }
+
+    @Override
+    public void stop() {
+        timer.cancel();
+    }
+}
---
+++ 02-timer-task/src/main/java/com/sample/module/timer/TaskService.java
@@ -0,0 +1,15 @@
+package com.sample.module.timer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.TimerTask;
+
+public class TaskService extends TimerTask {
+    private static final Logger logger = LoggerFactory.getLogger(TaskService.class);
+
+    @Override
+    public void run() {
+        logger.info("Scheduler service executed");
+    }
+}
